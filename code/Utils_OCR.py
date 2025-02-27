0c66dafcd6ee6545ee2d200adc4e4282011373da
UncleBang <740438587@qq.com>
2025-02-27 10:11:54 +0800

    图像OCR

--- code/Utils_OCR.py
+++ code/Utils_OCR.py
@@ -2,7 +2,7 @@ import pytesseract
 from PIL import Image
 
 # 加载图片
-image = Image.open("C:/Users/Yun/Desktop/Cache_-39c41bc39d332ff6.jpg")
+image = Image.open("C:/Users/Yun/Desktop/学习力表.jpg")
 
 # 进行OCR识别
 text = pytesseract.image_to_string(image, lang='chi_sim')  # 使用简体中文语言包
